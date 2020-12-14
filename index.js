30d7fcc1fcc439b62e4624d083b249d5fca00927
Kristoffer Tonning <kristoffer@helixsleep.com>
2020-12-14 16:03:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core')
+
+try {
+    const search = core.getInput('search');
+    console.log(`Searching "${search}" for Jira ticket ID.`)
+
+    const match = search.match(/([A-Za-z]{3}-\d{1,})/g)
+    const ticket_id = match ? match[0] : null
+    
+    console.log(`Found: ${ticket_id}`)
+} catch (error) {
+    core.setFailed(error.message);
+}
