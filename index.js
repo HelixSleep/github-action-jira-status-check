c1e523feab3ce1fd425230854979530118d4f400
Kristoffer Tonning <tonning@gmail.com>
2021-11-24 13:53:27 +0100

    Allow for more project IDs

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github')
 const JiraApi = require('jira-client')
 
-
 try {
     const issueNumberInput = core.getInput('issueNumber');
     const statusMatchInput = core.getInput('status');
-    
+
     const search = issueNumberInput ? issueNumberInput : github.context.payload.pull_request.head.ref;
     const statusMatch = statusMatchInput ? statusMatchInput : 'Under Code Review';
-    
+
     console.log(`Searching "${search}" for Jira issue number.`)
 
-    const match = search.match(/([A-Za-z]{3}-\d{1,})/g)
+    const match = search.match(/([A-Za-z]{2,4}-\d{1,})/g)
     const issueNumber = match ? match[0] : null
 
     if (!issueNumber) {
         console.log('No issue number found. Assuming ready.')
         return;
     }
-    
+
     console.log(`Issue number found: ${issueNumber}`)
     core.setOutput("issueNumber", issueNumber);
 
@@ -30,8 +29,8 @@ try {
         password: process.env.JIRA_API_TOKEN,
         apiVersion: '2',
         strictSSL: true
-    });    
-    
+    });
+
     jira.findIssue(issueNumber)
         .then(issue => {
             const statusFound = issue.fields.status.name;
